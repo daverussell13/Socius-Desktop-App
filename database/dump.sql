bcbe5774b1125423429b5654cf622bcd8ae0d7c0
Dave Russell <daverussell1309@gmail.com>
2023-01-17 01:49:56 +0700

    Fix: css fontface import

--- database/dump.sql
+++ database/dump.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `socius` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `socius`;
 -- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
 --
 -- Host: localhost    Database: socius
@@ -16,6 +18,65 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `events`
+--
+
+DROP TABLE IF EXISTS `events`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `events` (
+  `event_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `creator_id` int unsigned NOT NULL,
+  `event_name` varchar(45) NOT NULL,
+  `event_description` varchar(45) NOT NULL,
+  `event_date` date NOT NULL,
+  `event_time` time NOT NULL,
+  `event_organizer` varchar(45) DEFAULT NULL,
+  `total_joined_user` int unsigned DEFAULT '0',
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`event_id`),
+  UNIQUE KEY `event_id_UNIQUE` (`event_id`),
+  KEY `fk_event_user_idx` (`creator_id`),
+  CONSTRAINT `fk_event_user` FOREIGN KEY (`creator_id`) REFERENCES `users` (`user_id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `events`
+--
+
+LOCK TABLES `events` WRITE;
+/*!40000 ALTER TABLE `events` DISABLE KEYS */;
+/*!40000 ALTER TABLE `events` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_event`
+--
+
+DROP TABLE IF EXISTS `user_event`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_event` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int unsigned NOT NULL,
+  `event_id` int unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_event`
+--
+
+LOCK TABLES `user_event` WRITE;
+/*!40000 ALTER TABLE `user_event` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_event` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `users`
 --
 
@@ -27,12 +88,14 @@ CREATE TABLE `users` (
   `username` varchar(20) NOT NULL,
   `name` varchar(20) NOT NULL,
   `email` varchar(30) NOT NULL,
-  `password` varchar(30) NOT NULL,
-  `is_admin` int unsigned NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `is_admin` int unsigned DEFAULT '0',
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `deleted_at` datetime DEFAULT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `username_UNIQUE` (`username`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,6 +104,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (8,'test','test','test@gmail.com','$2y$12$gzeBLQ/bPDKy.r.GabtYL.5dFYQoDGtPaRSZeODgMj4W3tT9cXxYa',0,'2023-01-16 12:08:26',NULL),(9,'game','game','game@gmail.com','$2y$12$c/j3NWafOvnwzWik9xvL0O2TEr8.Q5lS6Jy62v2ce0BICZZqkS0MC',0,'2023-01-16 13:12:01',NULL),(10,'lol','lol','lol@gmail.com','$2y$12$fSwcHkrkSYeVB4sty0loF.zEpAUhGsVuDW7dO7zN4YZC.Xh.EB.oO',0,'2023-01-16 18:18:23',NULL),(11,'kenneth','kenneth','kenneth@gmail.com','$2y$12$fSwcHkrkSYeVB4sty0loF.zEpAUhGsVuDW7dO7zN4YZC.Xh.EB.oO',1,'2023-01-16 22:34:04',NULL);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -53,4 +117,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-01-16  3:40:00
+-- Dump completed on 2023-01-16 23:40:08
